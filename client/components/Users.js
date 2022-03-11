e06ed215c7b57033961a7f265fe3de7ba5c9d140
kwame gumbs <kwame.gumbs@gmail.com>
2022-03-10 21:24:33 -0500

    user update

---
+++ client/components/Users.js
@@ -0,0 +1,40 @@
+
+import React, { useState, useEffect } from "react";
+import { fetchUsers } from "../store/users";
+import { useDispatch, useSelector } from "react-redux";
+ import { Link, Redirect } from "react-router-dom";
+
+function Users(){
+
+  const  userList  = useSelector(state => state.allUsers );
+  const { isAdmin } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+   }, []);
+    return  isAdmin ? (
+      <div>
+        {userList.map((user) => (
+          <Link key={user.id} to={`/User/${user.id}`}>
+            <div>
+              <h1>User: {user.username}</h1>
+              <h3>Is Admin: {String(user.isAdmin)}</h3>
+              {!isAdmin? null : (
+                <>
+                  <button>edit</button>
+                  <button>delete</button>
+                </>
+              )}
+            </div>
+          </Link>
+        ))}
+      </div>
+        ):(<Redirect to="/home" />)
+    
+  }
+
+
+
+
+
+export default Users
---
+++ client/store/users.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+// ACTION TYPES
+const GOT_ALL_USERS = "GOT_ALL_USERS";
+
+// ACTION CREATORS
+const gotAllUsers = (USERS) => {
+  return {
+    type: GOT_ALL_USERS,
+    USERS,
+  };
+};
+
+// THUNK
+export const fetchUsers = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get("/api/users");
+      dispatch(gotAllUsers(data));
+    } catch (error) {
+      console.log("fetchNFTs error: ", error);
+    }
+  };
+};
+
+// REDUCER
+const initialState = [];
+export default function UserReducer(state = initialState, action) {
+  switch (action.type) {
+    case GOT_ALL_USERS:
+      return action.USERS;
+    default:
+      return state;
+  }
+}
