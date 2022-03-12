275e4e16df2f591b5d1d7f1825d2c640e5956b8c
Alberto Huerta <albertohuerta@albertohuertamac.lan>
2022-03-11 20:20:11 -0500

    connected about page to navbar/created search box in allproducts

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,55 +2,56 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
 
 const Navbar = ({ handleClick, isLoggedIn }) => {
   const dispatch = useDispatch();
-  const {username, isAdmin} = useSelector((state) => state.auth)
+  const { username, isAdmin } = useSelector((state) => state.auth);
 
-
-  
   return (
-      <div className="nav">
-        <div className="title">
-          <h1>Stooped NYC</h1>
-        </div>
-        <nav>
-          {!isLoggedIn ? (
-                    <div>
-                    {/* The navbar will show these links before you log in */}
-          
-                    <Link to="/products">Home</Link>
-                    <Link to="/products">Products</Link>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Sign Up</Link>
-                  </div>
-          ) : isAdmin ? (
-            <div>
-              <h2>hello Admin {username}</h2>
-              <Link to="/products">Home</Link>
-              <Link to="/admin/users">Users</Link>
-              <Link to="/products">Products</Link>
-              <Link to="/test"></Link>
-              <a href="#" onClick={handleClick}>
-                Logout
-              </a>
-            </div>
-          ) : (
-            <div>
-              <h2>Hello, {username}</h2>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <Link to="/products">Home</Link>
-              <a href="#" onClick={handleClick}>
-                Logout
-              </a>
-            </div>
-          )}
-        </nav>
-        <hr />
+    <div className="nav">
+      <div className="title">
+        <h1>Stooped NYC</h1>
       </div>
-    )
+      <nav>
+        {!isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links before you log in */}
+
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/products">Search</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        ) : isAdmin ? (
+          <div>
+            <h2>hello Admin {username}</h2>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/admin/users">Users</Link>
+            <Link to="/products">Search</Link>
+            <Link to="/test"></Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            <h2>Hello, {username}</h2>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/products">Search</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </div>
+  );
 };
 
 /**
