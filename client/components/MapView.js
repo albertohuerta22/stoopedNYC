e30d1d3d6f847ccd93fc90be5f07bee11e7042c7
Alberto Huerta <albertohuerta@albertohuertamac.lan>
2022-03-11 21:32:58 -0500

    added map with key/NOTE: need hide key

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from 'react-google-maps';
 
 function Map() {
   return (
     <GoogleMap
-      defaultZoom={10}
+      defaultZoom={12}
       defaultCenter={{ lat: 40.712776, lng: -74.005974 }}
     />
   );
@@ -17,9 +22,9 @@ const mapView = () => {
     <div>
       <div style={{ width: '100vw', height: '100vh' }}>
         <WrappedMap
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBzfXuSV3tMH-P0wHjs_29y9fIgbmvgJ8E`}
           loadingElement={<div style={{ height: '100%' }} />}
-          containerElement={<div style={{ height: '100%' }} />}
+          containerElement={<div style={{ height: '70%', width: '50%' }} />}
           mapElement={<div style={{ height: '100%' }} />}
         />
       </div>
