43820fe462a2231ea4814329f35a72652e176aff
Alberto Huerta <albertohuerta@albertohuertamac.lan>
2022-03-12 23:01:14 -0500

    created gitignore for api keys

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import {
   GoogleMap,
   withScriptjs,
@@ -8,22 +9,53 @@ import {
 
 function Map() {
   return (
-    <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: 40.712776, lng: -74.005974 }}
-    />
+    <div>
+      <h1>
+        Stooped!{' '}
+        <span role="img" aria-label="stoop">
+          📦
+        </span>
+      </h1>
+      <GoogleMap
+        defaultZoom={12}
+        defaultCenter={{ lat: 40.712776, lng: -74.005974 }}
+        onClick={(event) => {
+          setMarkers((current) => [
+            ...current,
+            {
+              lat: event.latLng.lat(),
+              lng: event.latLng.lng(),
+              time: new Date(),
+            },
+          ]);
+        }}
+      />
+      {MarkerClusterer.map((marker) => (
+        <Marker />
+      ))}
+    </div>
   );
 }
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
-
+// const API_KEY = process.env.REACT_APP_GOOGLE_KEY;
 const mapView = () => {
   return (
     <div>
       <h1>Find stooped items near you!</h1>
-      <div style={{ width: '100vw', height: '100vh' }}>
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
         <WrappedMap
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBzfXuSV3tMH-P0wHjs_29y9fIgbmvgJ8E`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=
+            AIzaSyBzfXuSV3tMH - P0wHjs_29y9fIgbmvgJ8E
+          `}
           loadingElement={<div style={{ height: '100%' }} />}
           containerElement={<div style={{ height: '70%', width: '50%' }} />}
           mapElement={<div style={{ height: '100%' }} />}
