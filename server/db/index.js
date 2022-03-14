097f45c44648b467778cfde07f4d4b49080fe1af
Alberto Huerta <albertohuerta@albertohuertamac.lan>
2022-03-13 23:37:12 -0400

    final push stackathon

--- server/db/index.js
+++ server/db/index.js
@@ -4,20 +4,15 @@ const db = require('./db');
 
 const User = require('./models/User');
 const Product = require('./models/Products');
-const Vote = require('./models/Vote');
 
 //associations could go here!
 // User.hasMany(Product);
 // Product.belongsTo(User);
-
-Product.hasOne(Vote);
-Vote.belongsTo(Product);
 
 module.exports = {
   db,
   models: {
     User,
     Product,
-    Vote,
   },
 };
